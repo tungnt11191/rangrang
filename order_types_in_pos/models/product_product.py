b4c52bd66a6d0ab45549c5a2a5299985bdf95f52
Odoo Best <thebestodoo@gmail.com>
2022-10-31 14:01:13 +0700

    fix relationship

--- order_types_in_pos/models/product_product.py
+++ order_types_in_pos/models/product_product.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 
 class Product(models.Model):
-    _inherit = "product.template"
+    _inherit = "product.product"
 
     order_type_map_ids = fields.One2many('product.product.order.type.map', 'product_id', string='Maps')
 
@@ -12,7 +12,7 @@ class Product(models.Model):
 class ProductProductOrderTypeMap(models.Model):
     _name = "product.product.order.type.map"
 
-    product_id = fields.Many2one('product.product', string="Product", required=True)
+    product_id = fields.Many2one('product.product', string="Product")
     delivery_type = fields.Many2one('delivery.type', string='Order Type', required=True)
     income_account_id = fields.Many2one('account.account', string="Income", required=True)
     expense_account_id = fields.Many2one('account.account', string="Expense", required=True)
@@ -20,3 +20,7 @@ class ProductProductOrderTypeMap(models.Model):
     _sql_constraints = [
         ('product_type_unique', 'unique (product_id, delivery_type)', 'The combination product/type already exists!'),
     ]
+
+    def create(self, vals_list):
+        res = super(ProductProductOrderTypeMap, self).create(vals_list)
+        return res
