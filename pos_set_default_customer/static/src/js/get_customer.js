e687d898771e59fc7a6dc5838f5a61088caaa557
Odoo Best <thebestodoo@gmail.com>
2022-11-17 08:43:33 +0700

    fix default get customer

--- pos_set_default_customer/static/src/js/get_customer.js
+++ pos_set_default_customer/static/src/js/get_customer.js
@@ -3,14 +3,13 @@ odoo.define('pos_set_default_customer.GetCustomer', function(require) {
 
     var models = require('point_of_sale.models');
 
-    var _super_order = models.Order.prototype;
-//    models.Order = models.Order.extend({
-//        initialize: function(attributes,options) {
-////            _super_order.initialize.apply(this, arguments, options);
-//            _super_order.initialize.call(this,attributes,options);
-//            if (this.pos.config.default_partner_id && !this.export_as_JSON().partner_id) {
-//            	this.set_client(this.pos.db.get_partner_by_id(this.pos.config.default_partner_id[0]));
-//            }
-//        },
-//    });
+    var _super = models.Order;
+    models.Order = models.Order.extend({
+        initialize: function(attributes,options){
+            _super.prototype.initialize.call(this,attributes,options);
+            if (this.pos.config.default_partner_id && !this.export_as_JSON().partner_id) {
+            	this.set_client(this.pos.db.get_partner_by_id(this.pos.config.default_partner_id[0]));
+            }
+        },
+    });
 });
