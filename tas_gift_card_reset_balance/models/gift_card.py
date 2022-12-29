60049aa5b3f9fe4f79a24a92862be1b75f5f2caf
Odoo Best <thebestodoo@gmail.com>
2022-12-29 15:47:57 +0700

    recompute balance of gift card

--- tas_gift_card_reset_balance/models/gift_card.py
+++ tas_gift_card_reset_balance/models/gift_card.py
@@ -3,7 +3,8 @@
 
 from odoo import api, fields, models, _
 import base64
-from datetime import datetime
+from datetime import datetime, timedelta
+from calendar import monthrange
 
 
 class GiftCard(models.Model):
@@ -17,7 +18,56 @@ class GiftCard(models.Model):
         today = (datetime.now()).date()
 
         if first_day_of_month == today:
-            cards = self.search([('reset_balance', '=', True)])
-            for card in cards:
-                if card.reset_balance:
-                    card.balance = card.initial_amount
+            cards = self.search([])
+            cards._compute_balance()
+            # cards = self.search([('reset_balance', '=', True)])
+            # for card in cards:
+            #     if card.reset_balance:
+            #         card.balance = card.initial_amount
+
+    @api.onchange('reset_balance')
+    def _onchange_reset_balance(self):
+        self._compute_balance()
+
+    @api.depends("redeem_pos_order_line_ids", "redeem_line_ids")
+    def _compute_balance(self):
+        for record in self:
+            if not self.reset_balance:
+                super()._compute_balance()
+            else:
+                first_day_of_month = (datetime.now().replace(hour=0, minute=0))
+
+                days_in_month = monthrange(first_day_of_month.year, first_day_of_month.month)[1]
+                first_day_of_next_month = (datetime.now().replace(hour=0, minute=0)) + timedelta(days=days_in_month)
+                confirmed_line = record.redeem_line_ids.filtered(
+                    lambda l: l.state in ('sale', 'done')
+                                and l.create_date >= first_day_of_month
+                                and l.create_date < first_day_of_next_month
+                )
+                balance = record.initial_amount
+                if confirmed_line:
+                    balance -= sum(confirmed_line.mapped(
+                        lambda line: line.currency_id._convert(line.price_unit, record.currency_id, record.env.company,
+                                                               line.create_date) * -1
+                    ))
+                record.balance = balance
+
+                confirmed_line = record.redeem_pos_order_line_ids.sudo().filtered(
+                    lambda l: l.order_id.state in ('paid', 'done', 'invoiced')
+                              and l.create_date >= first_day_of_month
+                              and l.create_date < first_day_of_next_month
+                )
+                balance = record.balance
+                if confirmed_line:
+                    balance -= sum(
+                        confirmed_line.mapped(
+                            lambda line: line.currency_id._convert(
+                                line.price_unit,
+                                record.currency_id,
+                                record.env.company,
+                                line.create_date,
+                            )
+                            * -1
+                        )
+                    )
+                record.balance = balance
