f156c78f1f5f50f166b55393ed90386c91878f91
Cao Thế Anh <caotheanh.buh@outlook.com>
2023-01-10 16:43:26 +0700

    ADD: sent email

---
+++ l10_vn_pos/models/gift_card.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+import base64
+
+from odoo import fields, api, models
+
+
+class GiftCard(models.Model):
+    _inherit = 'gift.card'
+
+    def action_sent_gift_card(self):
+        selected_ids = self.env.context.get('active_ids', [])
+        selected_records = self.env['gift.card'].browse(selected_ids)
+        for rec in selected_records:
+            mail_template = self.env.ref('sale_gift_card.mail_template_gift_card')
+            mail_template.send_mail(rec.id, force_send=True)
+
