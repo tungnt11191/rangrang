3c2828687c49d2c74ec391142cf9634e00dd8dd5
Cao Thế Anh <caotheanh.buh@outlook.com>
2023-01-10 12:26:00 +0700

    FIX: barcode partner

--- l10_vn_pos/models/res_partner.py
+++ l10_vn_pos/models/res_partner.py
@@ -10,21 +10,29 @@ class ResPartner(models.Model):
 
     barcode = fields.Char(help="Use a barcode to identify this contact.", copy=False, company_dependent=True)
 
+    @staticmethod
+    def get_barcode_customer(phone):
+        phone_str = str(phone)
+        phone_str = phone_str.replace("+", "")
+        phone_str = phone_str.replace(" ", "")
+        barcode = 'RRC' + phone_str
+        return barcode
+
     @api.model
     def create(self, vals):
         if vals.get('mobile'):
-            vals['barcode'] = 'RRC' + vals.get("mobile")
+            vals['barcode'] = self.get_barcode_customer(vals.get('mobile'))
         elif vals.get('phone'):
-            vals['barcode'] = 'RRC' + vals.get("phone")
+            vals['barcode'] = self.get_barcode_customer(vals.get('phone'))
         res = super(ResPartner, self).create(vals)
         return res
 
     def write(self, vals):
         res = super(ResPartner, self).write(vals)
         if vals.get("mobile"):
-            self.barcode = 'RRC' + vals.get("mobile")
+            self.barcode = self.get_barcode_customer(vals.get('mobile'))
         elif vals.get("phone"):
-            self.barcode = 'RRC' + vals.get("phone")
+            self.barcode = self.get_barcode_customer(vals.get('phone'))
         return res
 
 
