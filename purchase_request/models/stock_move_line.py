154a7bad973ab43828586428caf30a4faa07a165
Cao Thế Anh <caotheanh.buh@outlook.com>
2023-02-02 16:07:58 +0700

    FIX: remove email sent to vendor

--- purchase_request/models/stock_move_line.py
+++ purchase_request/models/stock_move_line.py
@@ -106,17 +106,18 @@ class StockMoveLine(models.Model):
                     message = self._purchase_request_confirm_done_message_content(
                         message_data
                     )
-                    request.message_post(
-                        body=message, subtype_id=self.env.ref("mail.mt_comment").id
-                    )
+                    # Remove email to vendor
+                    # request.message_post(
+                    #     body=message, subtype_id=self.env.ref("mail.mt_comment").id
+                    # )
 
                     picking_message = self._picking_confirm_done_message_content(
                         message_data
                     )
-                    ml.move_id.picking_id.message_post(
-                        body=picking_message,
-                        subtype_id=self.env.ref("mail.mt_comment").id,
-                    )
+                    # ml.move_id.picking_id.message_post(
+                    #     body=picking_message,
+                    #     subtype_id=self.env.ref("mail.mt_comment").id,
+                    # )
 
                 allocation._compute_open_product_qty()
 
