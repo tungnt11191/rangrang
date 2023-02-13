66bd6fbff70f42a02d22810f7eb52cfc1a2a8e7b
Cao Thế Anh <caotheanh.buh@outlook.com>
2023-02-13 12:43:20 +0700

    ADD: custom PO report

---
+++ tas_po_report/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': 'Purchase Order Report',
+    'summary': """Purchase Order Report Customize""",
+
+    'description': """
+       Custom PO report
+    """,
+    'author': "TASYS",
+    'website': "",
+    'category': 'tasys',
+    'version': '1.0',
+    'depends': ['purchase'],
+    'images': [
+    ],
+    'data': [
+        'report/purchase_order_report_custom.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'OEEL-1',
+}
