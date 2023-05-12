331dfde0efbf3db4aeda4454ff43d4c5ef19b463
Cao Thế Anh <caotheanh.buh@outlook.com>
2023-05-12 12:14:07 +0700

    Fix: receipt form loyalty

--- l10_vn_pos/__manifest__.py
+++ l10_vn_pos/__manifest__.py
@@ -9,7 +9,7 @@ TASYS POS Localization
 =======================================================
     """,
     'license': 'LGPL-3',
-    'depends': ['point_of_sale', 'pos_gift_card', 'sale', 'sale_coupon', 'pos_hr'],
+    'depends': ['point_of_sale', 'pos_gift_card', 'sale', 'sale_coupon', 'pos_hr', 'pos_loyalty'],
     'data': [
         'data/gift_card_data.xml',
         # 'data/activate.xml',
